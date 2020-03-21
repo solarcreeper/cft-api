0911f651c05801d6321200610ffad40a5be53979
yehongjiang <yehongjiang2012@gmail.com>
2020-03-21 16:40:15 +0800

    io

---
+++ vue/src/router/index.js
@@ -0,0 +1,47 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
+import IO from '../components/IO.vue'
+import Result from '../components/Result.vue'
+import Task from '../components/Task.vue'
+import Config from '../components/Config.vue'
+import Report from '../components/Report.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/login', component: Login },
+  { path: '/', redirect: '/login' },
+  {
+    path: '/home',
+    component: Home,
+    redirect: '/welcome',
+    children: [
+      { path: '/welcome', component: Welcome },
+      { path: '/io', component: IO },
+      { path: '/result', component: Result },
+      { path: '/task', component: Task },
+      { path: '/config', component: Config },
+      { path: '/report', component: Report }
+    ]
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') {
+    return next()
+  }
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) {
+    return next('/login')
+  }
+  next()
+})
+
+export default router
